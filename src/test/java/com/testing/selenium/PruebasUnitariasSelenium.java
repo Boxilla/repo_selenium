ace8c39acd486c4ec0c23cea438f80c54a1466a3
Alejandro Quintana Saravia <alejandro.quintana@sansano.usm.cl>
2021-08-09 12:25:36 -0400

    Update PruebasUnitariasSelenium.java

--- src/test/java/com/testing/selenium/PruebasUnitariasSelenium.java
+++ src/test/java/com/testing/selenium/PruebasUnitariasSelenium.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class PruebasUnitariasSelenium {
 	private WebDriver driver;
